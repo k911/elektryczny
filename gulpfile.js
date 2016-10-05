ad2db6de59b4f4ac953485b165307e87c5a12ede
k911 <konradobal@gmail.com>
2016-10-05 19:24:55 +0200

    Build process automation - almost final
    - Deleted embeded package "mdi" and changed to install as npm dependency
    - Updated mdi to 1.7.22
    - Hard-coded icon names in .scss changed to mdi(name) function from mdi
    package
    - Created simple instalation gulp task
    - Prepared for upcoming style update

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,87 @@
-/**
- * [Created by FeiryCod]
- */
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    livereload = require('gulp-livereload');
+	sourcemaps = require('gulp-sourcemaps'),
+	notify = require('gulp-notify'),
+	clean = require('gulp-clean');
 
-// Here you can specify the output style of .css file
-// as like the other options
-var Options = {
-    SassOutputStyle: {
-        outputStyle: 'compressed'
-    } //nested, expanded, compact, compressed (gulp-sass)
-
+/**
+ * @var array 'sassSrc' sass files included for compilation tasks 
+ * @var string 'sassDest' destination for compiled sass files
+ * @var gulp-sass.Options 'sassConfig'
+ */
+var settings = {
+	sassSrc: ['theme/*.scss', '!_*.scss'],
+	sassDest: 'theme',
+    sassConfig: {
+        outputStyle: 'compressed' // ['nested', 'expanded', 'compact', 'compressed']
+    },
+	defaultTasks: ['info', 'compile-sass:watch']
 };
 
-// Styles Tasks
-// Compile scss to css
-gulp.task('styles', function() {
+/**
+ * Initialize all needed for first time
+ */
+ gulp.task('init', ['copy-files', 'compile-sass']), function() {
+	console.log('Doing some things..');
+ }
 
-    console.log(Options.SassOutputStyle);
-    return gulp.src('theme/stylesheet.scss')
-        .pipe(sass(Options.SassOutputStyle).on('error', sass.logError))
-        .pipe(gulp.dest('theme'))
-        .pipe(livereload());
-
+/**
+ * Show default info
+ */
+gulp.task('info', function() {
+	console.log('\nCurrent settings:\n');
+	console.log(settings);
+	console.log('\n');
 });
 
-//Watch Task
-gulp.task('watch', function() {
-    livereload.listen();
-    gulp.watch('theme/styles/*.scss', ['styles']);
-    gulp.watch('views/*', function() {
-        livereload.reload('template/*');
-    });
-
+gulp.task('clean-files', function() {
+	return gulp.src('theme/fonts', {read: false})
+		.pipe(clean())
+		.pipe(notify('Deleted: <%= file.relative %>'));
 });
-gulp.task('default', ['styles', 'watch']);
+
+/**
+ * Copy files from installed dependencies
+ */
+gulp.task('copy-files', ['clean-files'], function() {
+	return gulp.src('node_modules/mdi/fonts/*', {base: 'node_modules/mdi/fonts/'})
+		.pipe(gulp.dest('theme/fonts'))
+		.pipe(notify('Copied dependecy: <%= file.relative %>'));
+});
+
+/**
+ * Compile all styles task
+ */
+gulp.task('compile-sass', function() {
+    return gulp.src(settings.sassSrc)
+        .pipe(sass(settings.sassConfig).on('error', sass.logError))
+        .pipe(gulp.dest(settings.sassDest))
+		.pipe(notify('Compiled, minified: <%= file.relative %>'));
+});
+
+/**
+ * Auto compile all styles task during changes
+ */
+gulp.task('compile-sass:watch', function() {
+	console.log('\nAuto-compiling enabled\n');
+	gulp.watch(['theme/*.scss', 'theme/styles/*.scss'], ['compile-sass']);
+});
+
+/**
+ * @experimental
+ * Compile all styles task with source maps
+ * TODO: in the future
+ */
+gulp.task('compile-sass-maps', function() {
+    return gulp.src(settings.sassSrc)
+		.pipe(sourcemaps.init())
+        .pipe(sass(settings.sassConfig).on('error', sass.logError))
+        .pipe(gulp.dest(settings.sassDest))
+		.pipe(sourcemaps.write('theme/maps'))
+		.pipe(notify('Compiled, minified and created sourcemap: <%= file.relative %>'));
+});
+
+/**
+ * @default tasks
+ */
+gulp.task('default', settings.defaultTasks);
