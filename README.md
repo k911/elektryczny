c530f35b7f28c86dfef7200d461bcbc57d1529de
k911 <kondzio9114@gmail.com>
2016-10-19 20:53:47 +0200

    Merged with 3.2.0-RC1 phpbb prosilver and refactored whole code.

--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 
 phpbb3 theme fork
 theme created for private purposes
+tested versions of phpbb:
+- 3.2.0-RC1
+theme will be maintained at least till phpbb release stable 3.2.0 version
 
 `<phpBB>` phpBB3 installation path (where file 'config.php' is located)
 
-## Installation
+## (optional) Installation
+
+#### Style is precompiled you do not have to use this instructions unless you want to edit it
 
 1. Clone this repository to:
 	`<phpBB>/styles/`
@@ -15,25 +20,28 @@ theme created for private purposes
 	`<phpBB>/styles/elektryczny/` 
 3. Run
 	`npm install`
-4. Then
+4. (optional) if you have not install gulp globally or you are not sure enter:
+	`npm install --global gulp`
+5. Then
 	`gulp init`
-5. Theme is ready to use.
+6. Theme is ready to use.
 
 ## Edit
 
 - You can find and edit SASS files in:
-	`<phpBB>/styles/elektryczny/theme/styles/`
+	`<phpBB>/styles/elektryczny/dev/styles/`
 	
 ## Compile
 
 - You can use your private sass compiler to compile *.scss files in 
-	`<phpBB>/styles/elektryczny/theme/`
+	`<phpBB>/styles/elektryczny/dev/`
 - Or use previously defined gulp task
 	- Open cmd at directory
 		`<phpBB>/styles/elektryczny/`
 	- Run `gulp compile-sass` to compile each *.scss files
-- You can also turn on 'auto-compilation
+- You can also turn on auto-compilation and auto-uglify js files
 	- Open cmd at directory
 		`<phpBB>/styles/elektryczny/`
-	- Run `gulp` or `gulp compile-sass:watch`
+	- Run `gulp` or `gulp compile-sass:watch` and `gulp uglify-js:watch`
 	- ..and you have turned on auto-compilation until you terminate cmd process
+- Command `gulp cleanup` removes whole stuff created by script `gulp init`
--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,36 @@
+// External dependencies
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
 	sourcemaps = require('gulp-sourcemaps'),
 	notify = require('gulp-notify'),
-	clean = require('gulp-clean');
+	clean = require('gulp-clean'),
+	uglify = require('gulp-uglify');
 
 /**
+ * @var string 'jsSrc' js files to uglify (minify)
+ * @var string 'jsDest' destination of the compiled js files
+ * @var array 'sassWatch' files that needs to be watched for auto-compiling sass
  * @var array 'sassSrc' sass files included for compilation tasks 
  * @var string 'sassDest' destination for compiled sass files
  * @var gulp-sass.Options 'sassConfig'
+ * @var array 'defaultTask' tasks run on 'default' task is invoked eg. when in cmd is typed only 'gulp'
  */
 var settings = {
-	sassSrc: ['theme/*.scss', '!_*.scss'],
+	jsSrc: 'src/javascript/*.js',
+	jsDest: 'template',
+	sassWatch: ['src/*.scss', 'src/styles/*.scss'],
+	sassSrc: ['src/*.scss', '!_*.scss'],
 	sassDest: 'theme',
     sassConfig: {
         outputStyle: 'compressed' // ['nested', 'expanded', 'compact', 'compressed']
     },
-	defaultTasks: ['info', 'compile-sass', 'compile-sass:watch']
+	defaultTasks: ['info', 'compile-sass:watch', 'uglify-js:watch']
 };
 
 /**
  * Initialize all needed for first time
  */
- gulp.task('init', ['copy-files', 'compile-sass']), function() {
+ gulp.task('init', ['copy-fonts', 'compile-sass', 'uglify-js']), function() {
 	console.log('Doing some things..');
  }
 
@@ -34,19 +43,33 @@ gulp.task('info', function() {
 	console.log('\n');
 });
 
-gulp.task('clean-files', function() {
+/**
+ * Deletes old folder with fonts
+ */
+gulp.task('clean-fonts', function() {
 	return gulp.src('theme/fonts', {read: false})
 		.pipe(clean())
 		.pipe(notify('Deleted: <%= file.relative %>'));
 });
 
 /**
- * Copy files from installed dependencies
+ * Copy mdi fonts from installed dependencies
  */
-gulp.task('copy-files', ['clean-files'], function() {
+gulp.task('copy-fonts', ['clean-fonts'], function() {
 	return gulp.src('node_modules/mdi/fonts/*', {base: 'node_modules/mdi/fonts/'})
 		.pipe(gulp.dest('theme/fonts'))
 		.pipe(notify('Copied dependecy: <%= file.relative %>'));
+});
+
+/**
+ * Task to do general cleanup in project
+ * Deletes all compiled, copied or minified/uglified files
+ */
+
+gulp.task('cleanup', ['clean-fonts'], function() {
+	return gulp.src(['theme/*.css', 'template/*.js'], {read: false})
+		.pipe(clean())
+		.pipe(notify('Deleted: <%= file.relative %>'));
 });
 
 /**
@@ -62,9 +85,27 @@ gulp.task('compile-sass', function() {
 /**
  * Auto compile all styles task during changes
  */
-gulp.task('compile-sass:watch', function() {
-	console.log('\nAuto-compiling enabled\n');
-	gulp.watch(['theme/*.scss', 'theme/styles/*.scss'], ['compile-sass']);
+gulp.task('compile-sass:watch', ['compile-sass'], function() {
+	console.log('\nAuto-compiling sass enabled\n');
+	gulp.watch(settings.sassWatch, ['compile-sass']);
+});
+
+/**
+ * Minify all javascript task
+ */
+gulp.task('uglify-js', function(){
+	return gulp.src(settings.jsSrc)
+		.pipe(uglify())
+		.pipe(gulp.dest(settings.jsDest))
+		.pipe(notify('Uglified: <%= file.relative %>'));;
+});
+
+/**
+ * Auto uglify javascript task during changes
+ */
+gulp.task('uglify-js:watch', ['uglify-js'], function() {
+	console.log('\nAuto-uglifing js enabled\n');
+	gulp.watch(settings.jsSrc, ['uglify-js']);
 });
 
 /**
@@ -84,4 +125,6 @@ gulp.task('compile-sass-maps', function() {
 /**
  * @default tasks
  */
-gulp.task('default', settings.defaultTasks);
+gulp.task('default', settings.defaultTasks, function() {
+	console.log('\nDeflaut tasks successfully invoked.\n');
+});
